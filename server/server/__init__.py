4d258927ea4c97617763135213a916d27bf8ca67
Reza <reza.machdi@web.de>
2019-09-19 21:28:08 +0200

    Added Pipfile dependencies

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -26,7 +26,7 @@ def create_app(test_config=None):
 
     # a simple page that says hello
     @app.route('/')
-    def hello():
-        return 'Hello, World!'
+    def index():
+        return 'Index and debug'
 
     return app
